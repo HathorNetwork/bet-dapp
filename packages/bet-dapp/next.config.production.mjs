f1fb3b778d2d373cc6b32e899ca85a734a62d692
André Abadesso <andre.abadesso@gmail.com>
2025-01-16 09:41:16 -0300

    chore: correct url in production config

--- packages/bet-dapp/next.config.production.mjs
+++ packages/bet-dapp/next.config.production.mjs
@@ -2,9 +2,26 @@
 const nextConfig = {
   output: "standalone",
   basePath: "",
-  assetPrefix: "https://staging.betting.hathor.network/",
+  assetPrefix: "https://betting.hathor.network/",
+  async rewrites() {
+    return [
+      {
+        source: '/fonts/:path*',
+        destination: '/public/fonts/:path*',
+      },
+    ];
+  },
   webpack: config => {
     config.externals.push('pino-pretty', 'lokijs', 'encoding');
+
+    // Add module rule for font files
+    config.module.rules.push({
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      type: 'asset/resource',
+      generator: {
+        filename: 'static/fonts/[name][ext]'
+      }
+    });
 
     return {
       ...config,
